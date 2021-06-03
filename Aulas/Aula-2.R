7822dd1a95f86ec91a3b464661e49e993c5c8497
Claudiano Neto <cneto@ufrb.edu.br>
2021-06-02 22:49:32 -0300

    conteudo da aula 2

---
+++ Aulas/Aula-2.R
@@ -0,0 +1,66 @@
+#######  AULA 2
+#   dutra.andeise@gmail.com
+
+# Shapefile ---------------------------------------------------------------
+# install.packages(c("sf", "sp", "geobr"))
+
+library(sp)
+library(sf)
+library(geobr)
+
+
+dados <- geobr::list_geobr()
+
+municipio <- geobr::read_municipality(code_muni = 3505104, year = 2020)
+
+shape <- sf::as_Spatial(municipio$geom)
+
+mascara <- sp::spTransform(shape, CRS(
+  "+proj=utm +zone=22 +datum=WGS84 +units=m +no_defs"))
+
+# Raster ------------------------------------------------------------------
+
+pasta <- file.path(getwd(), "arquivos") # criando caminho e nome pra pasta
+dir.create(pasta) # criando pasta
+
+# remotes::install_github("16EAGLE/getSpatialData") # instalando pacote 
+
+library(getSpatialData)
+library(tidyverse)
+
+login_USGS(username = "claudianoneto", password = "marilia91020") # login
+
+produtos <- getSpatialData::getLandsat_products() # atribuindo produtos do Landsat
+produtos
+
+landsat <- getLandsat_records(
+                        time_range = c("2020-08-01", "2020-08-15"),
+                        products = "landsat_8_c1",
+                        aoi = mascara )
+
+orbita <- landsat[which(landsat$tile_number_vertical == 75), ]   # visualizando imagem 
+
+install.packages("magrittr")
+library(magrittr)
+
+orbita <- orbita %>%    # criando coluna summary porq pct requer isso
+          mutate(summary = "NA")
+
+orbita <- landsat %>%  # filtrando "tile"
+          filter(tile_number_vertical == 75)
+
+# criar 
+visualizar <- get_previews(records = orbita, # configurar imagem para visualizacao
+                           level = "l1", # ta disponivel 0800 ... sr nao ta 
+                           dir_out = pasta)
+# visualizar
+plot_previews(records = visualizar[2,],
+              show_aoi = shape)
+
+verificar <- check_availability(orbita) # verificar disponibilidade
+pedido <- order_data(orbita[c(5,21),])   # realizar pedido 
+download <- getLandsat_data(records = orbita[c(5,21),], dir_out <-pasta)
+
+
+
+# FAZER O DOWNLOAD DA AREA DE ESTUDO DA MINHA CIDADE

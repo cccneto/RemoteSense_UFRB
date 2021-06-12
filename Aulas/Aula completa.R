0d6ff392759559f7af0d552a5d26ea69e25ca389
Claudiano Neto <cneto@ufrb.edu.br>
2021-06-11 21:28:25 -0300

    AULA 6

--- Aulas/Aula completa.R
+++ Aulas/Aula completa.R
@@ -14,7 +14,6 @@ library(ggplot2)
 library(ggspatial)
 library(rgdal)
 library(arulesViz)
-library(getSpatialData)
 library(tidyverse)
 
 
@@ -33,10 +32,12 @@ mascara <- sp::spTransform(shape, CRS(
 # Raster ------------------------------------------------------------------
 
 pasta <- file.path(getwd(), "arquivos") # criando caminho e nome pra pasta
+
 dir.create(pasta) # criando pasta
 
-# remotes::install_github("16EAGLE/getSpatialData") # instalando pacote 
-
+# instalar o pacote  
+# remotes::install_github("16EAGLE/getSpatialData") 
+library(getSpatialData)
 
 
 login_USGS(username = "claudianoneto", password = "marilia91020") # login
@@ -106,7 +107,8 @@ plot(landsat_imagem) # plotando a imagem
 # criando nossa mascara 
 mascara <- spTransform(shape, CRS("+proj=utm +zone=22 +datum=WGS84 +units=m +no_defs"))
 
-# plotando a mascara em cima da banda4 
+# plotando a mascara em cima da banda4
+# stack deve ter a mesma extensao
 plot(mascara, add = T)
 
 
@@ -186,19 +188,19 @@ plot(bloco2)
 # plot dos rggbb
 raster::plotRGB(
                 bloco1, 4, 3, 2, 
-                scale = 32767, 
+                scale = 0.5,  # 0.5 porque ja ta reescalonado - verificar number float
                 stretch = "lin", 
                 colNA = "white"
                 )
 
 raster::plotRGB(
                 bloco2, 4, 3, 2, 
-                scale = 32767, 
+                scale = 0.5, 
                 stretch = "lin", 
                 colNA = "white"
                 )
 
-# unido as imagens do meu mosaico
+# unindo as imagens do meu mosaico
 mosaico <- raster::mosaic(
                           bloco1, 
                           bloco2,
@@ -206,7 +208,7 @@ mosaico <- raster::mosaic(
                           )
 
 raster::plotRGB(mosaico, 4, 3, 2, 
-                scale = 32767, 
+                scale = 0.5, 
                 stretch = "lin", # contraste 
                 colNA = "white")
 
@@ -239,7 +241,7 @@ mosaico_semnuvem <- raster::mask(mosaico, # aplicando a nova mascara no mosaico
                                  mascara_qualidade)   
 
 raster::plotRGB(mosaico_semnuvem, 4, 3, 2, # plotando mosaico sem nuvem
-                scale = 0.5, 
+                scale = 0.5,  # 
                 stretch = "lin", 
                 colNA = "white"
                                 )
@@ -262,3 +264,176 @@ declividade <- raster::terrain(raster_elevacao,
                                )   
 
 plot(declividade)
+
+raster::writeRaster(declividade,
+                    "./arquivos/declividade.tif")
+
+###########################################
+
+################   Aula 5
+
+# Reclassificar -----------------------------------------------------------
+
+library(elevatr)
+library(raster)
+
+declividade <- raster::raster("./arquivos/declividade.tif")
+
+classes <- matrix(c(-Inf, 2, 1,
+                    2, 5, 2,
+                    5, 25, 3,
+                    25, 50, 4,
+                    50, Inf, 5),
+                  ncol = 3,
+                  byrow = T)
+
+declividade_classificado <- raster::reclassify(declividade,
+                                               classes)
+
+municipio <- geobr::read_municipality(code_muni = 3505104, year = 2020)
+
+shape <- sf::as_Spatial(municipio) 
+mascara <- spTransform(shape, CRS("+proj=utm +zone=22 +south +datum=WGS84 +units=m +no_defs"))
+
+plot(declividade_classificado)
+plot(mascara, add = T)
+
+recortar <- crop(declividade_classificado, mascara)
+declividade_area <- mask(recortar, mascara)
+plot(declividade_area)
+
+
+declividade_df <- as.data.frame(declividade_area,
+                                xy = T) %>% drop_na()
+
+# renomear nossas classes
+
+declividade_df$declividade <- factor(declividade_df$declividade,
+                                     levels = c(1, 2, 3, 4, 5),
+                                     labels = c("Plano", "Suave Ondulado",
+                                                "Ondulado", "Montanhoso",
+                                                "Escarpado"))
+head(declividade_df)
+
+
+
+
+n <- ggplot2::ggplot() + 
+  geom_raster(aes(x = x, y = y, fill = declividade), data = declividade_df) + 
+  #scale_fill_brewer(type = "seq", palette = "Greys")
+  scale_fill_brewer(palette = "RdYlGn", name = "Classes", direction = -1) +
+  labs(title = "Grafico de Declividade") + 
+  theme_bw()
+
+n + annotation_scale(location = "bl", width_hint = .4, plot_unit = "m") +
+  annotation_north_arrow(location = "bl", which_north = "true",
+                         pad_x = unit(0.2, "in"), pad_y = unit(0.4, "in"),
+                         style = north_arrow_fancy_orienteering)
+#Para exportar a figura do ultimo plot
+ggsave("./arquivos/Declividade.png", dpi = 100, width = 15, height = 10, units = "in")
+
+
+###########################################
+################   Aula 6
+
+library(raster)
+library(sp)
+library(sf)
+
+municipio <- geobr::read_municipality(code_muni = 3505104, year = 2020)
+shape <- sf::as_Spatial(municipio) 
+mascara <- sp::spTransform(shape, CRS("+proj=utm +zone=22 +south +datum=WGS84 +units=m +no_defs"))
+
+# criando lista de arquivos
+arq <- list.files("./arquivos/NDVI_2020/", pattern = ".tif$", full.names = T)
+arq
+
+NDVI_bloco <- raster::stack(arq)
+plot(NDVI_bloco$LC08_221075_20200614)
+plot(mascara, add = T)
+
+# O t1 retorna o valor de um pixel para cada camada que está dentro do ndvi_bloco
+t1 <- NDVI_bloco[980, 870]
+# quero transformar t1 em valor numerico
+t2 <- as.numeric(t1)
+
+# como podemos plotar isso?
+serie <- stats::ts(t2, start = c(2020, 1), 
+                   end = c(2020, 18),
+                   frequency = 18) # temos 18 arquivos 
+
+plot(serie)
+
+# automatizando atraves da funcao for
+
+# isso serve para permitir que todo 
+# resultado do loop e for seja guardado aqui dentro
+
+outlist <- list()
+media_mes <- list()
+
+for (i in 1:length(NDVI_bloco)) {
+  dado <- NDVI_bloco[[i]] 
+  
+  recortado <- crop(dado, mascara)
+  area_estudo <- mask (recortado, mascara)
+  plot(area_estudo)
+  
+  outlist[[i]] <- area_estudo
+  
+  valores <- getValues(area_estudo)
+  media <- mean(valores, na.rm =T)
+  
+  media_mes[[i]] <- media
+  
+}
+
+class(outlist)
+NDVI_bloco <- 
+
+class(outlist)
+NDVI_recortado <- raster::stack(outlist)
+class(NDVI_recortado)
+
+media_mes
+t3 <- as.numeric(media_mes)
+serie_ndvi <- ts(t3, start = c(2020,1), end = c(2020,18), frequency = 18)
+plot(serie_ndvi) # plotando a media das 18 imagens para o municipio todo
+
+media_anual_ndvi <- raster::calc(NDVI_recortado,fun = mean, na.rm = T)
+plot(media_anual_ndvi)
+
+sd_anual_ndvi <- raster::calc(NDVI_recortado, fun = sd, na.rm = T)
+plot(sd_anual_ndvi)
+
+mediana_anual_ndvi <- raster::calc(NDVI_recortado,fun = median, na.rm = T)
+plot(mediana_anual_ndvi)
+
+writeRaster(media_anual_ndvi, "./arquivos/NDVI_2020/Media_anual_ndvi.tif")
+writeRaster(sd_anual_ndvi, "./arquivos/NDVI_2020/SD_anual_ndvi.tif")
+writeRaster(mediana_anual_ndvi, "./arquivos/NDVI_2020/Mediana_anual_ndvi.tif")
+
+###### KRIGAGEM
+
+library(geoR)
+
+parana <- geoR::parana
+summary(parana)
+class(parana)
+
+x <- c(1, 3, 4, 5, 6)
+y <- c(4, 7, 8, 8, 9)
+z <- c(5,6,7, 9, 10)
+plot(x, y, pch = "+", cex = z/4)
+
+teste <- as.geodata(cbind(x, y, z))
+plot(teste)
+
+plot(parana)
+
+parana_vario <- variog(parana)
+plot(parana_vario)
+
+parana.ml1 <- likfit(parana, trend = "1st", ini = c(1000, 50),
+                     nug = 100)
+summary(parana.ml1)
